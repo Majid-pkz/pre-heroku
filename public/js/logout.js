c5d7c10fc8288af84e48ce3eec8b19ff97d72456
Majid-pkz <majid.computing@gmail.com>
2023-03-07 13:59:44 +1100

    make logout active again and remove all css

--- public/js/logout.js
+++ public/js/logout.js
@@ -11,4 +11,4 @@ const logout = async()=> {
   }
 };
 // note: where is logout button?
-// document.querySelector('#logout').addEventListener('click', logout);
+ document.querySelector('#logout').addEventListener('click', logout);
